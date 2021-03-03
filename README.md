43f2058288e0f2a007642ced4ae545a9ca3c5688
Jan Horacek <me@apophis.cz>
2021-03-03 16:32:32 +0100

    Add development & production settings.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Finálová úloha Logiky N-Trophy 2021.
+# Finálová úloha Logiky N-Trophy 2021
+
+## Howto
+
+### Initialize environment
+
+```bash
+$ virtualenv -p python3 venv
+$ source venv/bin/activate
+$ pip3 install -r requirements.txt
+```
+
+### Create db structure
+
+```bash
+$ ./manage.py migrate --settings=pastvina.settings.development
+$ ./manage.py makemigrations pastvina --settings=pastvina.settings.development
+$ ./manage.py migrate pastvina --settings=pastvina.settings.development
+```
+
+### Run development server
+
+```bash
+$ cp pastvina/settings/development.py.txt pastvina/settings/development.py
+Edit pastvina/settings/development.py as you wish.
+$ ./manage.py collectstatic --settings=pastvina.settings.development
+$ ./manage.py runserver --settings=pastvina.settings.development
+```
+
+### Deploy to production
+
+```bash
+$ cp pastvina/settings/production.py.txt pastvina/settings/production.py
+Edit pastvina/settings/development.py as you wish.
+$ ./manage.py collectstatic --settings=pastvina.settings.production
+```
--- pastvina/settings/base.py
+++ pastvina/settings/base.py
@@ -15,19 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$+8w72^%n@%1q*7^9+e+99je6r)6wbta@q#dgb*cv@d*7y8z8('
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['localhost']
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -74,17 +61,6 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.fallback.FallbackStorage'
 WSGI_APPLICATION = 'pastvina.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -111,15 +87,11 @@ LANGUAGE_CODE = 'cs'
 
 LANGUAGES = (
     ('cs', u'Čeština'),
-    ('en', u'English'),
 )
 
 TIME_ZONE = 'Europe/Prague'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -140,7 +112,11 @@ PRIVATE_ROOT = os.path.join(STORAGE_ROOT, PRIVATE_DIR_NAME)
 AUTH_USER_MODEL = 'pastvina.User'
 LOGIN_URL = '/login/'
 
-try:
-    from local_settings import *
-except:
-    pass
+
+# Email
+
+EMAIL_PORT = 465
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.onebit.cz'
+EMAIL_HOST_USER = 'logika-send@ntrophy.cz'
+EMAIL_USE_SSL = True
