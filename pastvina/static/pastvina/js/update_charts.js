68e4e4ffc2befed2aa00a242e297f7d26f295717
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2021-03-05 18:09:05 +0100

    Add updating charts from json

---
+++ pastvina/static/pastvina/js/update_charts.js
@@ -0,0 +1,47 @@
+var cropProductionCharts = new Map();
+var cropStorageCharts = new Map();
+var livestockProductionCharts = new Map();
+
+function update_charts(updateChartsJson) {
+    let updateInfo = JSON.parse(updateChartsJson);
+    console.log(updateInfo);
+
+    for (crop of updateInfo.crops) {
+        document.getElementById("crop-buy-price-" + crop.id).innerHTML = crop.buy;
+        document.getElementById("crop-sell-price-" + crop.id).innerHTML = crop.sell;
+
+        // Production
+        let productionChart = cropProductionCharts[crop.id];
+        for (let i = 0; i < crop.production.length; i++) {
+            productionChart.data.datasets[0].data[i] = crop.production[i];
+        }
+        for (let i = crop.production.length; i < productionChart.data.datasets[0].data.length; i++) {
+            productionChart.data.datasets[0].data[i] = 0;
+        }
+        productionChart.update();
+
+        // Storage
+        let storageChart = cropStorageCharts[crop.id];
+        for (let i = 0; i < crop.storage.length; i++) {
+            storageChart.data.datasets[0].data[i] = crop.storage[i];
+        }
+        for (let i = crop.storage.length; i < storageChart.data.datasets[0].data.length; i++) {
+            storageChart.data.datasets[0].data[i] = 0;
+        }
+        storageChart.update();
+    }
+
+    for (ls of update_info.livestock) {
+        document.getElementById("ls-buy-price-" + ls.id).innerHTML = ls.buy;
+        document.getElementById("ls-sell-price-" + ls.id).innerHTML = ls.sell;
+
+        let productionChart = livestockProductionCharts[ls.id];
+        for (let i = 0; i < ls.production.length; i++) {
+            productionChart.data.datasets[0].data[i] = ls.production[i];
+        }
+        for (let i = ls.production.length; i < productionChart.data.datasets[0].data.length; i++) {
+            productionChart.data.datasets[0].data[i] = 0;
+        }
+        productionChart.update();
+    }
+}
