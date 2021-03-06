779c8f939de9802cf22c5f9ea6813eb81b57c7fe
Jan Horacek <me@apophis.cz>
2021-03-06 14:36:53 +0100

    [Cron] Add TODOs.

--- pastvina/cron/main.py
+++ pastvina/cron/main.py
@@ -6,11 +6,25 @@ Run it manully to make single tisk.
 
 
 def rounds_update() -> None:
-    from pastvina.models import Round
     from pastvina.cron.round import new as new_round
+
+    # TODO: check if new round should be started, call round.new
+    # Call when round is started according to time, but no tick is in database
+    # This function creates new tick.
+
+    pass
+
+
+def ticks_update() -> None:
+    from pastvina.models import Round
+
     from pastvina.cron.tick import new as new_tick
 
     # TODO: not working
+    # Calculate ticck according to time of round, if tick does not exist in
+    # db, create it and call new_tick.
+    # Db maybe should be locked between request and new tick creation.
+    # This function creates new tick.
     for round_ in Round.objects.all():
         if round_.is_running():
             new_round(round_, round_.tick())
@@ -18,3 +32,4 @@ def rounds_update() -> None:
 
 if __name__ in ['__main__', 'django.core.management.commands.shell']:
     rounds_update()
+    ticks_update()
