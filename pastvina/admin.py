f0b43e31cbbb3b768799ceb90ac17168b0d855c2
Jan Horacek <me@apophis.cz>
2021-03-06 10:29:59 +0100

    Add models to admin.

--- pastvina/admin.py
+++ pastvina/admin.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from . import widgets
-from .models import Contribution, MediaFile, Crop, Livestock
+from .models import *
 from django.contrib import admin
 
 
@@ -27,3 +27,34 @@ class CropAdmin(admin.ModelAdmin):
 @admin.register(Livestock)
 class CropAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'base_price_buy', 'base_price_sell', 'growth_time', 'color']
+
+
+@admin.register(Round)
+class RoundAdmin(admin.ModelAdmin):
+    list_display = ['id', 'start', 'ticks', 'period', 'crop_storage_size', 'livestock_slaughter_limit', 'start_money']
+
+
+@admin.register(TeamCropHistory)
+class CromHistoryAdmin(admin.ModelAdmin):
+    list_display = ['round', 'user', 'tick', 'crop', 'age', 'amount']
+
+
+@admin.register(TeamLivestockHistory)
+class TeamLivestockHistoryAdmin(admin.ModelAdmin):
+    list_display = ['round', 'user', 'tick', 'livestock', 'age', 'amount']
+
+
+@admin.register(TeamHistory)
+class TeamHistoryAdmin(admin.ModelAdmin):
+    list_display = ['round', 'user', 'tick', 'money']
+
+
+@admin.register(CropMarketHistory)
+class CropMarketHistoryAdmin(admin.ModelAdmin):
+    list_display = ['round', 'tick', 'crop', 'amount_sold', 'current_price_buy', 'current_price_sell']
+
+
+@admin.register(LivestockMarketHistory)
+class LivestockMarketHistoryADmin(admin.ModelAdmin):
+    list_display = ['round', 'tick', 'livestock', 'amount_sold', 'current_price_buy',
+                    'current_price_sell', 'product_amount_sold', 'product_current_price']
