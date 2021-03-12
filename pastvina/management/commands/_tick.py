38ee92650e9d7674b735ba2303903f254dfdac34
Martin Kurecka <kur3cka.martin@gmail.com>
2021-03-12 23:14:29 +0100

    update prices

--- pastvina/management/commands/_tick.py
+++ pastvina/management/commands/_tick.py
@@ -1,3 +1,6 @@
+from functools import reduce
+from math import exp
+
 from pastvina.models import Round, Tick, Livestock, Crop, TeamCropHistory, TeamLivestockHistory, CropMarketHistory, \
     LivestockMarketHistory, TeamHistory
 
@@ -17,15 +20,37 @@ def cpy_to_next_tick(arr, new_tick):
     return arr
 
 
+def update_prices(arr, price_field, amount_field, coef_to_price):
+    s = reduce(lambda x, y: x + abs(getattr(y, amount_field)), arr, 0)
+
+    vals = [exp(- 1 - getattr(a, amount_field) / (s + 1)) for a in arr]
+    exp_s = sum(vals)
+    coefs = [len(arr) * v / exp_s for v in vals]
+
+    for i, a in enumerate(arr):
+        setattr(a, price_field, coef_to_price(a, coefs[i]))
+
+
 def new(tick: Tick, new_tick: Tick) -> None:
     crops_states = {crop.crop.id: crop for crop in CropMarketHistory.objects.filter(tick=tick).select_related('crop')}
     livestock_states = {ls.livestock.id: ls for ls in LivestockMarketHistory.objects.filter(tick=tick).select_related('livestock')}
 
     team_states = {team.user.id: team for team in TeamHistory.objects.filter(tick=tick)}
     team_cons_and_prod = {key: {'cons': 0, 'prod': 0} for key in team_states.keys()}
+    team_crop_capacity = {key: tick.round.crop_storage_size for key in team_states.keys()}
 
-    team_crops = TeamCropHistory.objects.filter(tick=tick)
-    TeamCropHistory.objects.bulk_create(apply_aging(team_crops, new_tick))
+    team_crops = TeamCropHistory.objects.filter(tick=tick).order_by('-age')
+    team_crops = apply_aging(team_crops, new_tick)
+    team_crops_in_storage = []
+    for crop in team_crops:
+        if crop.age <= crops_states[crop.crop.id].crop.rotting_time:
+            amount_stored = min(team_crop_capacity[crop.user.id], crop.amount)
+            crop.amount = amount_stored
+            team_crop_capacity[crop.user.id] -= amount_stored
+
+        team_crops_in_storage.append(crop)
+
+    TeamCropHistory.objects.bulk_create(team_crops_in_storage)
 
     team_livestock = TeamLivestockHistory.objects.filter(tick=tick)
 
@@ -49,5 +74,24 @@ def new(tick: Tick, new_tick: Tick) -> None:
             team_state.money += team_cons_and_prod[team_state.user.id]['prod']
 
     TeamHistory.objects.bulk_create(cpy_to_next_tick([team_state for team_state in team_states.values()], new_tick))
-    CropMarketHistory.objects.bulk_create(cpy_to_next_tick(crops_states.values(), new_tick))
-    LivestockMarketHistory.objects.bulk_create(cpy_to_next_tick(livestock_states.values(), new_tick))
+
+    next_crops_states = cpy_to_next_tick(crops_states.values(), new_tick)
+    update_prices(next_crops_states, 'current_price_sell', 'amount_sold',
+                  lambda a, coef: a.crop.base_price_sell * coef)
+    update_prices(next_crops_states, 'current_price_buy', 'amount_sold',
+                  lambda a, coef: a.crop.base_price_buy * coef)
+    for crop in next_crops_states:
+        crop.amount_sold *= 0.7
+    CropMarketHistory.objects.bulk_create(next_crops_states)
+
+    next_livestock_states = cpy_to_next_tick(livestock_states.values(), new_tick)
+    update_prices(next_livestock_states, 'current_price_sell', 'amount_sold',
+                  lambda a, coef: a.livestock.base_price_sell * coef)
+    update_prices(next_livestock_states, 'current_price_buy', 'amount_sold',
+                  lambda a, coef: a.livestock.base_price_buy * coef)
+    update_prices(next_livestock_states, 'product_current_price', 'product_amount_sold',
+                  lambda a, coef: a.livestock.product_price * coef)
+    for ls in next_livestock_states:
+        ls.amount_sold *= 0.7
+        ls.product_amount_sold *= 0.7
+    LivestockMarketHistory.objects.bulk_create(next_livestock_states)
