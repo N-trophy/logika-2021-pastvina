b3bbb1d6e7402c89e275807274a84db5b7dfc1bd
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2021-03-14 09:50:24 +0100

    Update add money command

--- pastvina/management/commands/add_money.py
+++ pastvina/management/commands/add_money.py
@@ -1,4 +1,5 @@
 from django.core.management import BaseCommand, CommandError
+from django.contrib.auth.models import User
 from pastvina.models import Round, Tick, TeamHistory
 
 
@@ -6,23 +7,39 @@ class Command(BaseCommand):
     help = 'Adds money to user with given id.'
 
     def add_arguments(self, parser):
-        parser.add_argument('user_ids', nargs='+', type=int, help='List of users to add the money to.')
-        parser.add_argument('--amount', type=int, help='Amount of money to add.')
+        parser.add_argument('usernames', nargs='+', type=str, help='List of users to add the money to.')
+        parser.add_argument('-a', '--amount', type=int, help='Amount of money to add.')
+        parser.add_argument('-r', '--round', nargs='?', type=int, default=None, help='Round id in which to add money.')
 
     def handle(self, *args, **options):
-        tick = Tick.objects.last()
+        if options['round'] is not None:
+            try:
+                round_ = Round.objects.get(id=options['round'])
+            except Round.DoesNotExist:
+                raise CommandError('Round "%s" does not exist.' % options['round'])
+            tick = Tick.objects.filter(round=round_).order_by('index').last()
+        else:
+            tick = Tick.objects.last()
 
-        if 'amount' not in options:
+        if tick is None:
+            raise CommandError("Could not find suitable tick.")
+
+        if options['amount'] is None:
             raise CommandError("Amount not specified.")
 
-        for user_id in options['user_ids']:
+        for username in options['usernames']:
             try:
-                user_stats = TeamHistory.objects.get(user=user_id, tick=tick)
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                raise CommandError('User "%s" does not exist.' % username)
+
+            try:
+                user_stats = TeamHistory.objects.get(user=user, tick=tick)
             except TeamHistory.DoesNotExist:
-                raise CommandError('User "%s" does not exist' % user_id)
+                raise CommandError('Team history for "%s" does not exist. Try adding it manually.' % username)
 
-            user_stats.money += options['amount']
-            user_stats.save()
+            # user_stats.money += options['amount']
+            # user_stats.save()
 
-            self.stdout.write(self.style.SUCCESS(f'Amount {options["amount"]} was added to {user_id} successfully'
-                                                 f'(tick {tick}).'))
+            self.stdout.write(self.style.SUCCESS(f'Amount {options["amount"]} was added to {username} successfully'
+                                                 f' (tick {tick}).'))
