3b867f36fc98ed27a47e71306c1820c7c3693f68
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2021-03-19 19:18:33 +0100

    Revert 63e3e9e6 and 9e257f9d

--- pastvina/static/pastvina/js/update_charts.js
+++ pastvina/static/pastvina/js/update_charts.js
@@ -114,7 +114,6 @@ function updateCharts(updateData) {
         }
         $("#crop-growing-amount-" + crop.id).text(growing);
         $("#crop-rotting-amount-" + crop.id).text(rotting);
-        $('.action-button').prop('disabled', false);
         $("#crop-buy-limit-" + crop.id).text(maxBuy);
         $("#crop-sell-limit-" + crop.id).text(rotting);
         $("#buy-crop-count-" + crop.id).attr({ "max": maxBuy });
